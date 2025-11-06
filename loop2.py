7f220af2ff000c9e491ef5eb576fdbe46ce1e329
kuldeep-bytes <kushwahkdk@gmail.com>
2025-11-06 14:55:34 +0530

    Loop que 2

---
+++ loop2.py
@@ -0,0 +1,9 @@
+#WAP to print the table of given number.
+num=int(input("Enter a number:" ))
+print("the table of entered number ",num," is")
+i=1
+while i<=10:
+    table=num*i
+    print(table)
+    i+=1
+    
