0db79e60040597e25ea83a513a51aafa41ea2cbd
kuldeep-bytes <kushwahkdk@gmail.com>
2025-11-10 20:34:15 +0530

    Palindrome number

---
+++ loop4.py
@@ -0,0 +1,13 @@
+# Check if a string is palindrome
+num=int(input("Enter a number: "))
+temp=num
+rev = 0
+while num>0:
+    digit = num%10
+    rev = rev*10+digit
+    num //=10
+if temp == rev:
+    print("Palindrome number.")
+else:
+    print("Not palindrome.")
+
